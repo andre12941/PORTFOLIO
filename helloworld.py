7071446d0c76b679ec6248ddb638bebc04c8da31
andre12941 <61563288+andre12941@users.noreply.github.com>
2026-06-27 16:20:12 +0200

    Inicios en Python

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+# This is a simple Python program that prints "Hello World!" to the console.
+print("Hello \nWorld!")
+sentence = 'the dog is named sammy'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('a'))
